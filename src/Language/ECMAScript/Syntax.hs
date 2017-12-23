f795d43e84004db53f49115d9010f3d45890faa7
qsctr <qsctr@users.noreply.github.com>
2017-12-23 20:48:47 +0800

    Add Language.ECMAScript.Syntax.LiteralExpression

--- src/Language/ECMAScript/Syntax.hs
+++ src/Language/ECMAScript/Syntax.hs
@@ -217,7 +217,8 @@ instance ToJSON VariableDeclarator where
         , "init" .= value ]
 
 data Expression
-    = ThisExpression
+    = LiteralExpression Literal
+    | ThisExpression
     | ArrayExpression [Maybe Expression]
     | ObjectExpression [Property]
     | FunctionExpression (Maybe Identifier) [Pattern] Block
@@ -234,6 +235,7 @@ data Expression
     | SequenceExpression [Expression]
 
 instance ToJSON Expression where
+    toJSON (LiteralExpression literal) = toJSON literal
     toJSON ThisExpression = node "ThisExpression" []
     toJSON (ArrayExpression elements) = node "ArrayExpression"
         [ "elements" .= elements ]
