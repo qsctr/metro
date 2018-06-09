a21764db7ac5e635c7640b9abacdb5cee9d3aff2
qsctr <qsctr@users.noreply.github.com>
2018-06-09 19:10:45 +0800

    Add MultiCase simplification step

---
+++ src/Language/Dtfpl/Simplify/MultiCase.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
+module Language.Dtfpl.Simplify.MultiCase () where
+
+import Language.Dtfpl.Simplify.Sim
+import Language.Dtfpl.Syntax
+
+instance Sim CaseHead 'MultiCase where
+    sim (CaseHead x) = CaseHead . T . pure <$> sim x
+
+instance Sim CaseAlt 'MultiCase where
+    sim (CaseAlt pat expr) = CaseAlt . T . pure <$> sim pat <*> sim expr
