fcfb5dee7c054aedd7e9d95cfa0f89e8d7e71c41
qsctr <qsctr@users.noreply.github.com>
2018-06-09 18:53:51 +0800

    Change Pass parameter for Sim to target type

--- src/Language/Dtfpl/Simplify.hs
+++ src/Language/Dtfpl/Simplify.hs
@@ -8,8 +8,8 @@
 {-# LANGUAGE TypeOperators         #-}
 {-# LANGUAGE UndecidableInstances  #-}
 
-module Language.Dtfpl.Simplify
-    (
+module Language.Dtfpl.Simplify.Sim
+    ( Sim (..)
     ) where
 
 import           Control.Monad.State
@@ -21,11 +21,11 @@ import           Language.Dtfpl.Syntax
 type SimState = State Int
 
 class Sim (n :: Node) (p :: Pass) where
-    sim :: n p -> SimState (n (Succ p))
+    sim :: n (Pred p) -> SimState (n p)
 
 type AutoSim (n :: Node) (p :: Pass) =
     ( SimChildren (Children n p) p
-    , SameChildren (Children n p) (Children n (Succ p)) )
+    , SameChildren (Children n (Pred p)) (Children n p) )
 
 type family SimChildren (ns :: [Node]) (p :: Pass) where
     SimChildren '[n] p = Sim n p
@@ -35,7 +35,7 @@ type family SameChildren (ns :: [Node]) (sns :: [Node]) where
     SameChildren '[n] '[sn] = n ~ sn
     SameChildren (n : ns) (sn : sns) = (n ~ sn, SameChildren ns sns)
 
-instance (Sim n p, Ann p ~ Ann (Succ p)) => Sim (A n) p where
+instance (Sim n p, Ann (Pred p) ~ Ann p) => Sim (A n) p where
     sim (A n a) = flip A a <$> sim n
 
 instance (Sim n p, Traversable t) => Sim (T t n) p where
@@ -66,13 +66,13 @@ instance AutoSim Expr p => Sim Expr p where
 instance {-# OVERLAPPABLE #-} AutoSim CaseHead p => Sim CaseHead p where
     sim (CaseHead x) = CaseHead <$> sim x
 
-instance Sim CaseHead 'Source where
+instance Sim CaseHead 'MultiCase where
     sim (CaseHead x) = CaseHead . T . pure <$> sim x
 
 instance {-# OVERLAPPABLE #-} AutoSim CaseAlt p => Sim CaseAlt p where
     sim (CaseAlt altHead expr) = CaseAlt <$> sim altHead <*> sim expr
 
-instance Sim CaseAlt 'Source where
+instance Sim CaseAlt 'MultiCase where
     sim (CaseAlt pat expr) = CaseAlt . T . pure <$> sim pat <*> sim expr
 
 instance Sim Ident p where
