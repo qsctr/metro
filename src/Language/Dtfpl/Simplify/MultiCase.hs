66d26ad2d5a647f6c706b2c89d7d23455c9c3a34
qsctr <qsctr@users.noreply.github.com>
2018-06-10 17:38:47 +0800

    Formatting

--- src/Language/Dtfpl/Simplify/MultiCase.hs
+++ src/Language/Dtfpl/Simplify/MultiCase.hs
@@ -1,10 +1,10 @@
-{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DataKinds             #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 
 module Language.Dtfpl.Simplify.MultiCase () where
 
-import Language.Dtfpl.Simplify.Sim
-import Language.Dtfpl.Syntax
+import           Language.Dtfpl.Simplify.Sim
+import           Language.Dtfpl.Syntax
 
 instance Sim CaseHead 'MultiCase where
     sim (CaseHead x) = CaseHead . T . pure <$> sim x
