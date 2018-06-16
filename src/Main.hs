020a89de619aa78941fa503ccc20b86d64e227aa
qsctr <qsctr@users.noreply.github.com>
2018-06-16 11:53:27 +0800

    Add CLI program

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,13 @@
 module Main where
 
-import Language.Dtfpl
+import qualified Data.Text.IO       as T
+import           Language.Dtfpl
+import           System.Environment
 
 main :: IO ()
-main = undefined
+main = do
+    filename <- head <$> getArgs
+    program <- readFile $ filename ++ ".dtfpl"
+    case compile (Config { debug = True }) program of
+        Left err -> putStrLn $ unlines $ showErr err
+        Right js -> js >>= T.writeFile (filename ++ ".js")
