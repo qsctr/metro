204c0c3f1f0d5ea36208254c063bb5599680c1a0
qsctr <qsctr@users.noreply.github.com>
2018-06-16 11:53:05 +0800

    Add compile function

--- src/Language/Dtfpl.hs
+++ src/Language/Dtfpl.hs
@@ -1,9 +1,26 @@
-module Language.Dtfpl where
+module Language.Dtfpl
+    ( module Language.Dtfpl.Config
+    , module Language.Dtfpl.Err
+    , module Language.Dtfpl.Err.ShowErr
+    , compile
+    ) where
 
-import Language.Dtfpl.Parser
-import Language.Dtfpl.Simplify
--- import Language.Dtfpl.Generate.Convert
--- import Language.Dtfpl.Generate.Render
+import           Control.Category
+import           Control.Monad.Except
+import           Control.Monad.Reader
+import           Data.Text                       (Text)
 
-compile :: String -> IO String
-compile = undefined
+import           Language.Dtfpl.Config
+import           Language.Dtfpl.Err
+import           Language.Dtfpl.Err.ShowErr
+import           Language.Dtfpl.Generate.Convert
+import           Language.Dtfpl.Generate.Render
+import           Language.Dtfpl.M
+import           Language.Dtfpl.Parser
+import           Language.Dtfpl.Simplify
+
+compile :: Config -> String -> Either Err (IO Text)
+compile config program = runReader (runExceptT (compileM program)) config
+
+compileM :: String -> M (IO Text)
+compileM = parse "" >=> simplify >>> convert >>> fmap render
