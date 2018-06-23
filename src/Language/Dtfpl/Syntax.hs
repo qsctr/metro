9d6c90f2b3521da4f90bab3b6f48a4f9fe4465d7
qsctr <qsctr@users.noreply.github.com>
2018-06-23 18:40:52 -0400

    Remove Data/Typeable instances for AST

--- src/Language/Dtfpl/Syntax.hs
+++ src/Language/Dtfpl/Syntax.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE ConstraintKinds           #-}
 {-# LANGUAGE DataKinds                 #-}
-{-# LANGUAGE DeriveDataTypeable        #-}
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE FlexibleContexts          #-}
 {-# LANGUAGE MultiParamTypeClasses     #-}
@@ -43,7 +42,6 @@ module Language.Dtfpl.Syntax
     , Lit (..)
     ) where
 
-import           Data.Data
 import           Data.Kind
 import           Data.List.NonEmpty
 import           Data.Promotion.Prelude      ((:==$))
@@ -72,10 +70,10 @@ type Node = Pass -> Type
 
 type Class = Type -> Constraint
 
-newtype P t (p :: Pass) = P t deriving (Eq, Show, Typeable, Data)
+newtype P t (p :: Pass) = P t deriving (Eq, Show)
 
 newtype T (t :: * -> *) (n :: Node) (p :: Pass) = T { unT :: t (n p) }
-    deriving (Eq, Show, Typeable, Data)
+    deriving (Eq, Show)
 
 data WhenAlt = forall t. WhenAlt Pass t
 
@@ -100,12 +98,9 @@ type family ToConstraint (c :: Class) (ns :: [Node]) (p :: Pass) where
     ToConstraint c (n : ns) p = (c (n p), ToConstraint c ns p)
 
 data A (n :: Node) (p :: Pass) = A { node :: n p, ann :: Ann p }
-    deriving Typeable
 
 deriving instance (Eq (n p), Eq (Ann p)) => Eq (A n p)
 deriving instance (Show (n p), Show (Ann p)) => Show (A n p)
-deriving instance (Typeable n, Typeable p, Data (n p), Data (Ann p))
-    => Data (A n p)
 
 type Ann (p :: Pass) = When p
     Loc
@@ -117,19 +112,17 @@ mapNode f (A n a) = A (f n) a
 genLoc :: Ann p ~ Maybe a => n p -> A n p
 genLoc = flip A Nothing
 
-newtype Prog (p :: Pass) = Prog (T [] (A Decl) p) deriving Typeable
+newtype Prog (p :: Pass) = Prog (T [] (A Decl) p)
 
 type instance Children Prog p =
     '[ T [] (A Decl) ]
 
 deriving instance Forall Eq Prog p => Eq (Prog p)
 deriving instance Forall Show Prog p => Show (Prog p)
-deriving instance (Forall Data Prog p, Typeable p) => Data (Prog p)
 
 data Decl (p :: Pass)
     = Def (DefHead p p) (DefBody p p)
     | Let (A Ident p) (A Expr p)
-    deriving Typeable
 
 type instance Children Decl p =
     '[ DefHead p, DefBody p
@@ -137,27 +130,23 @@ type instance Children Decl p =
 
 deriving instance Forall Eq Decl p => Eq (Decl p)
 deriving instance Forall Show Decl p => Show (Decl p)
-deriving instance (Forall Data Decl p, Typeable p) => Data (Decl p)
 
 type DefHead (p :: Pass) = VoidAfter 'NoDef p (A Ident)
 type DefBody (p :: Pass) = VoidAfter 'NoDef p (T NonEmpty (A DefAlt))
 
 data DefAlt (p :: Pass)
     = DefAlt (T NonEmpty (A Pat) p) (A Expr p)
-    deriving Typeable
 
 type instance Children DefAlt p =
     '[ T NonEmpty (A Pat), A Expr ]
 
 deriving instance Forall Eq DefAlt p => Eq (DefAlt p)
 deriving instance Forall Show DefAlt p => Show (DefAlt p)
-deriving instance (Forall Data DefAlt p, Typeable p) => Data (DefAlt p)
 
 data Pat (p :: Pass)
     = VarPat (A Ident p)
     | LitPat (A Lit p)
     | WildPat
-    deriving Typeable
 
 type instance Children Pat p =
     '[ A Ident
@@ -165,7 +154,6 @@ type instance Children Pat p =
 
 deriving instance Forall Eq Pat p => Eq (Pat p)
 deriving instance Forall Show Pat p => Show (Pat p)
-deriving instance (Forall Data Pat p, Typeable p) => Data (Pat p)
 
 data Expr (p :: Pass)
     = VarExpr (A Ident p)
@@ -174,7 +162,6 @@ data Expr (p :: Pass)
     | If (A Expr p) (A Expr p) (A Expr p)
     | Case (CaseHead p) (T NonEmpty (A CaseAlt) p)
     | LamExpr (Lam p)
-    deriving Typeable
 
 type instance Children Expr p =
     '[ A Ident
@@ -185,16 +172,14 @@ type instance Children Expr p =
 
 deriving instance Forall Eq Expr p => Eq (Expr p)
 deriving instance Forall Show Expr p => Show (Expr p)
-deriving instance (Forall Data Expr p, Typeable p) => Data (Expr p)
 
-newtype CaseHead (p :: Pass) = CaseHead (CaseHead' p p) deriving Typeable
+newtype CaseHead (p :: Pass) = CaseHead (CaseHead' p p)
 
 type instance Children CaseHead p =
     '[ CaseHead' p ]
 
 deriving instance Forall Eq CaseHead p => Eq (CaseHead p)
 deriving instance Forall Show CaseHead p => Show (CaseHead p)
-deriving instance (Forall Data CaseHead p, Typeable p) => Data (CaseHead p)
 
 type CaseHead' (p :: Pass) = When p
     (A Expr)
@@ -203,25 +188,21 @@ type CaseHead' (p :: Pass) = When p
 
 data AliExpr (p :: Pass)
     = AliExpr (A Expr p) (T Maybe Ident p)
-    deriving Typeable
 
 type instance Children AliExpr p =
     '[ A Expr, T Maybe Ident ]
 
 deriving instance Forall Eq AliExpr p => Eq (AliExpr p)
 deriving instance Forall Show AliExpr p => Show (AliExpr p)
-deriving instance (Forall Data AliExpr p, Typeable p) => Data (AliExpr p)
 
 data CaseAlt (p :: Pass)
     = CaseAlt (CaseAltHead p p) (A Expr p)
-    deriving Typeable
 
 type instance Children CaseAlt p =
     '[ CaseAltHead p, A Expr ]
 
 deriving instance Forall Eq CaseAlt p => Eq (CaseAlt p)
 deriving instance Forall Show CaseAlt p => Show (CaseAlt p)
-deriving instance (Forall Data CaseAlt p, Typeable p) => Data (CaseAlt p)
 
 type CaseAltHead (p :: Pass) = When p
     (A Pat)
@@ -229,14 +210,12 @@ type CaseAltHead (p :: Pass) = When p
 
 data Lam (p :: Pass)
     = Lam (LamHead p p) (A Expr p)
-    deriving Typeable
 
 type instance Children Lam p =
     '[ LamHead p, A Expr ]
 
 deriving instance Forall Eq Lam p => Eq (Lam p)
 deriving instance Forall Show Lam p => Show (Lam p)
-deriving instance (Forall Data Lam p, Typeable p) => Data (Lam p)
 
 type LamHead (p :: Pass) = When p
     (T NonEmpty (A Pat))
@@ -247,7 +226,6 @@ data Ident (p :: Pass)
     = Ident String
     | GenIdentPart (GenIdentPartPrefix p p) (GenIdentPartNum p p)
     | GenIdentFull (GenIdentFullNum p p)
-    deriving Typeable
 
 type instance Children Ident p =
     '[ GenIdentPartPrefix p, GenIdentPartNum p
@@ -255,7 +233,6 @@ type instance Children Ident p =
 
 deriving instance Forall Eq Ident p => Eq (Ident p)
 deriving instance Forall Show Ident p => Show (Ident p)
-deriving instance (Forall Data Ident p, Typeable p) => Data (Ident p)
 
 type GenIdentPartPrefix (p :: Pass) = FromInitGen p (A Ident)
 type GenIdentPartNum (p :: Pass) = FromInitGen p (P Natural)
@@ -264,4 +241,4 @@ type GenIdentFullNum (p :: Pass) = FromInitGen p (P Natural)
 data Lit (p :: Pass)
     = NumLit Double
     | StrLit String
-    deriving (Eq, Show, Typeable, Data)
+    deriving (Eq, Show)
