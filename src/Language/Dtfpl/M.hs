454f1d428cda23c2c10b578b060df6786e0009b2
qsctr <qsctr@users.noreply.github.com>
2018-06-23 23:09:12 -0400

    Rearrange declarations in Language.Dtfpl.M

--- src/Language/Dtfpl/M.hs
+++ src/Language/Dtfpl/M.hs
@@ -4,9 +4,9 @@
 
 -- | The core monad 'M' and associated constraints.
 module Language.Dtfpl.M
-    ( M (..)
-    , MError
+    ( MError
     , MConfig
+    , M (..)
     ) where
 
 import           Control.Monad.Except
@@ -15,15 +15,15 @@ import           Control.Monad.Reader
 import           Language.Dtfpl.Config
 import           Language.Dtfpl.Err
 
+-- | Monad that can throw 'Err'.
+type MError = MonadError Err
+
+-- | Monad that can read 'Config'.
+type MConfig = MonadReader Config
+
 -- | The core monad transformer stack.
 -- 
 -- - ExceptT for signaling errors
 -- - Reader for accessing config
 newtype M a = M { runM :: ExceptT Err (Reader Config) a }
     deriving (Functor, Applicative, Monad, MError, MConfig)
-
--- | Monad that can throw 'Err'.
-type MError = MonadError Err
-
--- | Monad that can read 'Config'.
-type MConfig = MonadReader Config
