f2d44f1dcfc31bec139d637a4d66d61876572b93
qsctr <qsctr@users.noreply.github.com>
2018-06-23 23:09:59 -0400

    Make parser functions polymorphic

--- src/Language/Dtfpl/Parser.hs
+++ src/Language/Dtfpl/Parser.hs
@@ -1,5 +1,8 @@
-{-# LANGUAGE DataKinds    #-}
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE ConstraintKinds           #-}
+{-# LANGUAGE DataKinds                 #-}
+{-# LANGUAGE FlexibleContexts          #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE TypeFamilies              #-}
 
 module Language.Dtfpl.Parser
     ( parse
@@ -7,11 +10,11 @@ module Language.Dtfpl.Parser
 
 import           Control.Category                  ((>>>))
 import           Control.Monad.Except
-import           Control.Monad.Reader
 import           Control.Monad.State
 import           Data.Bifunctor
 import           Data.Char
 import           Data.Functor
+import           Data.Functor.Identity
 import           Data.List
 import           Data.List.NonEmpty                (NonEmpty (..), some1, (<|))
 import           Text.Megaparsec                   hiding (parse)
@@ -19,53 +22,58 @@ import           Text.Megaparsec.Char
 import           Text.Megaparsec.Char.Lexer        (indentGuard, indentLevel,
                                                     nonIndented)
 
-import           Language.Dtfpl.Config
 import           Language.Dtfpl.Err
 import           Language.Dtfpl.M
 import           Language.Dtfpl.Parser.CustomError
 import           Language.Dtfpl.Parser.Loc
 import           Language.Dtfpl.Syntax
 
-type Parser = ParsecT CustomError String (StateT Pos (Reader Config))
+-- | Parser that has parsec functionality.
+type PParsec = MonadParsec CustomError String
 
-parse :: String -> String -> M (A Prog 'Source)
-parse filename input = M $ ExceptT $
+-- | Parser that keeps track of the current indentation level
+-- from the start of the line.
+type PIndentState = MonadState Pos
+
+parse :: MError m => String -> String -> m (A Prog 'Source)
+parse filename input = liftEither =<<
     first ParseErr <$> evalStateT (runParserT prog filename input) pos1
 
-testParse :: String -> Either String (A Prog 'Source)
-testParse input = first parseErrorPretty $
-    runReader (evalStateT (runParserT prog "" input) pos1)
-        Config { debug = True }
+_testParse :: String -> Either String (A Prog 'Source)
+_testParse input = first parseErrorPretty $
+    runIdentity $ evalStateT (runParserT prog "" input) pos1
 
-prog :: Parser (A Prog 'Source)
+prog :: (PParsec p, PIndentState p) => p (A Prog 'Source)
 prog = addLoc (Prog . T <$> many (nonIndented scn decl <* scn)) <* eof
 
-decl :: Parser (A Decl 'Source)
+decl :: (PParsec p, PIndentState p) => p (A Decl 'Source)
 decl = def <|> let_
   where def = addLoc $ Def <$> (lexeme sdef *> ident) <*> indentBlock' defAlt
         let_ = addLoc $ exprBlockMid $
             lexeme slet *> (Let <$> lexeme ident <* equals)
 
-defAlt :: Parser (A DefAlt 'Source)
-defAlt = addLoc $ exprBlockMid $ DefAlt . T <$> (some1 (lexeme $ try pat) <* arrow)
+defAlt :: (PParsec p, PIndentState p) => p (A DefAlt 'Source)
+defAlt = addLoc $ exprBlockMid $
+    DefAlt . T <$> (some1 (lexeme $ try pat) <* arrow)
 
-pat :: Parser (A Pat 'Source)
+pat :: PParsec p => p (A Pat 'Source)
 pat = varPat <|> litPat
   where varPat = addLoc $ VarPat <$> ident
         litPat = addLoc $ LitPat <$> literal
 
-exprBlockMid :: Parser (A Expr 'Source -> a) -> Parser a
+exprBlockMid :: (PParsec p, PIndentState p) => p (A Expr 'Source -> a) -> p a
 exprBlockMid = exprBlockWith indentLevel
 
-exprBlockStart :: Parser (A Expr 'Source -> a) -> Parser a
+exprBlockStart :: (PParsec p, PIndentState p) => p (A Expr 'Source -> a) -> p a
 exprBlockStart = exprBlockWith get
 
-exprBlockWith :: Parser Pos -> Parser (A Expr 'Source -> a) -> Parser a
+exprBlockWith :: (PParsec p, PIndentState p) =>
+    p Pos -> p (A Expr 'Source -> a) -> p a
 exprBlockWith ip p = do
     i <- ip
     p <*> (isc i *> expr i)
 
-expr :: Pos -> Parser (A Expr 'Source)
+expr :: (PParsec p, PIndentState p) => Pos -> p (A Expr 'Source)
 expr i = foldl1' combine <$> term `sepEndBy1` try sc1'
   where combine f x = A (App f x) $ Loc (start (ann f)) (end (ann x))
         term = varExpr <|> if_ <|> case_ <|> lam <|> litExpr <|> par
@@ -84,34 +92,34 @@ expr i = foldl1' combine <$> term `sepEndBy1` try sc1'
         sc' = isc i
         sc1' = isc1 i
 
-caseAlt :: Parser (A CaseAlt 'Source)
+caseAlt :: (PParsec p, PIndentState p) => p (A CaseAlt 'Source)
 caseAlt = addLoc $ exprBlockMid $ CaseAlt <$> (lexeme pat <* arrow)
 
 reservedWords :: [String]
 reservedWords = ["def", "let", "if", "then", "else", "case", "of"]
 
-sdef, slet, sif, sthen, selse, scase, sof :: Parser ()
+sdef, slet, sif, sthen, selse, scase, sof :: PParsec p => p ()
 [sdef, slet, sif, sthen, selse, scase, sof] = map
     (string >>> (*> notFollowedBy (satisfy isIdentTailChar)))
     reservedWords
 
-arrow :: Parser ()
+arrow :: PParsec p => p ()
 arrow = void $ string "->"
 
-equals :: Parser ()
+equals :: PParsec p => p ()
 equals = void $ string "="
 
-lambda :: Parser ()
+lambda :: PParsec p => p ()
 lambda = void $ string "\\"
 
 {-# ANN reservedChars "HLint: ignore Use String" #-}
 reservedChars :: [Char]
 reservedChars = "()[]{}.,:;\\\""
 
-parens :: Parser a -> Parser a
+parens :: PParsec p => p a -> p a
 parens = between (string "(") (string ")")
 
-ident :: Parser (A Ident 'Source)
+ident :: PParsec p => p (A Ident 'Source)
 ident = addLoc $ Ident <$> do
     identifier <- (:) <$> letterChar <*> takeWhileP Nothing isIdentTailChar
     if identifier `elem` reservedWords
@@ -123,7 +131,7 @@ isIdentTailChar x = isPrint x
                  && not (isSeparator x)
                  && x `notElem` reservedChars
 
-literal :: Parser (A Lit 'Source)
+literal :: PParsec p => p (A Lit 'Source)
 literal = numLit <|> strLit
   where numLit = addLoc $ NumLit . read <$>
             (option id ((:) <$> char '-') <*>
@@ -138,41 +146,42 @@ literal = numLit <|> strLit
                     , char '"'
                     , char 'n' $> '\n' ]
 
-addLoc :: Parser (n 'Source) -> Parser (A n 'Source)
+addLoc :: PParsec p => p (n 'Source) -> p (A n 'Source)
 addLoc p = do
     s <- getPosition
     A <$> p <*> (Loc s <$> getPosition)
 
-indentBlock' :: Parser (n p) -> Parser (T NonEmpty n p)
+indentBlock' :: (PParsec p, PIndentState p) =>
+    p (n 'Source) -> p (T NonEmpty n 'Source)
 indentBlock' p = T <$> (get >>= isc1 >>= rest)
   where rest i' = do
             a <- p
             hasNext *> ((a <|) <$> rest i') <|> noMore $> a :| []
           where hasNext = try $ indentGuard scn1 EQ i'
                 noMore = lookAhead $
-                    try (scn *> eof) <|> (void $ indentGuard scn1 LT i')
+                    try (scn *> eof) <|> void (indentGuard scn1 LT i')
 
-sc :: Parser ()
+sc :: PParsec p => p ()
 sc = hidden $ void $ takeWhile1P Nothing (== ' ')
 
-scn :: Parser ()
+scn :: (PParsec p, PIndentState p) => p ()
 scn = scnWith space
 
-scn1 :: Parser ()
+scn1 :: (PParsec p, PIndentState p) => p ()
 scn1 = scnWith space1
 
-scnWith :: Parser () -> Parser ()
+scnWith :: (PParsec p, PIndentState p) => p () -> p ()
 scnWith p = do
     s <- getPosition
     hidden p
     e <- getPosition
     when (sourceLine s /= sourceLine e) $ put $ sourceColumn e
 
-isc :: Pos -> Parser Pos
+isc :: (PParsec p, PIndentState p) => Pos -> p Pos
 isc = indentGuard scn GT
 
-isc1 :: Pos -> Parser Pos
+isc1 :: (PParsec p, PIndentState p) => Pos -> p Pos
 isc1 = indentGuard scn1 GT
 
-lexeme :: Parser a -> Parser a
+lexeme :: PParsec p => p a -> p a
 lexeme = (<* sc)
