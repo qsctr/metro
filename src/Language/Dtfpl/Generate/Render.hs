3aee3011bcc9ac4e0bdb46d74a14790b1bef4d47
qsctr <qsctr@users.noreply.github.com>
2018-07-24 16:29:24 -0400

    Add haddock for render phase

--- src/Language/Dtfpl/Generate/Render.hs
+++ src/Language/Dtfpl/Generate/Render.hs
@@ -1,3 +1,5 @@
+-- | Render a JS AST to JS source code.
+-- Just a wrapper around astring library in JavaScript.
 module Language.Dtfpl.Generate.Render
     ( render
     ) where
@@ -13,11 +15,18 @@ import           System.Process.Typed
 
 import           Language.ECMAScript.Syntax
 
+-- | Node process running js/main
 processConfig :: ProcessConfig Handle Handle ()
 processConfig = setStdin createPipe
               $ setStdout createPipe
               $ proc "node" ["js/main"]
 
+-- | Render a JS AST to JS source code.
+-- Starts a node process which runs js/main,
+-- then sends the JS AST in JSON in estree format to the node script,
+-- which uses astring to convert into string.
+-- The node process then encodes the resulting string in json and sends it back.
+-- Requires IO since it needs to deal with processes.
 render :: Program -> IO Text
 render program = withProcess processConfig $ \p -> do
     L.hPut (getStdin p) $ encode program `C.snoc` '\n'
