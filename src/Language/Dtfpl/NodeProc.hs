3937d0245a6e793d8989289e7c844da1050960d2
qsctr <qsctr@users.noreply.github.com>
2018-10-24 12:53:01 +0800

    Fix indentation

--- src/Language/Dtfpl/NodeProc.hs
+++ src/Language/Dtfpl/NodeProc.hs
@@ -11,5 +11,5 @@ type NodeProc = Process Handle Handle ()
 -- | Node process running js/main
 nodeProcConfig :: ProcessConfig Handle Handle ()
 nodeProcConfig = setStdin createPipe
-              $ setStdout createPipe
-              $ proc "node" ["js/main"]
+               $ setStdout createPipe
+               $ proc "node" ["js/main"]
