b8c2641d48208368bd77a272a61669d2fdb38660
Bretton <qsctr@users.noreply.github.com>
2019-12-26 16:11:16 +0800

    Add Language.Dtfpl.Parser.Loc.formatLoc
    and make the existing Show instance for Loc a synonym of it

--- src/Language/Dtfpl/Parser/Loc.hs
+++ src/Language/Dtfpl/Parser/Loc.hs
@@ -1,8 +1,9 @@
-{-# LANGUAGE RecordWildCards    #-}
+{-# LANGUAGE RecordWildCards #-}
 
 -- | Source locations for annotating nodes.
 module Language.Dtfpl.Parser.Loc
     ( Loc (..)
+    , formatLoc
     ) where
 
 import           Text.Megaparsec.Pos
@@ -11,7 +12,10 @@ import           Text.Megaparsec.Pos
 -- Represented as a range.
 data Loc = Loc { start :: SourcePos, end :: SourcePos } deriving Eq
 
-instance Show Loc where
-    show Loc {..} = showSourcePos start ++ "-" ++ showSourcePos end
-      where showSourcePos SourcePos {..} =
-                show (unPos sourceLine) ++ ":" ++ show (unPos sourceColumn)
+instance Show Loc where show = formatLoc
+
+-- | Display a 'Loc' in a concise readable format
+formatLoc :: Loc -> String
+formatLoc Loc {..} = formatSourcePos start ++ "-" ++ formatSourcePos end
+  where formatSourcePos SourcePos {..} =
+            show (unPos sourceLine) ++ ":" ++ show (unPos sourceColumn)
